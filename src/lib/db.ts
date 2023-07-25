2a449b720287cd4cafea4640f06d1a96d5821f03
Shubham Talan <shubhamsinghtalan08@gmail.com>
2023-07-25 23:09:27 +0530

    fix(db): added missing api keys

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,7 @@
-import { Redis } from "@upstash/redis/nodejs";
+import { Redis } from "@upstash/redis";
+import { getEnvVariable } from "./utils";
 
-export const db = Redis.fromEnv();
+export const db = new Redis({
+  url: getEnvVariable(`UPSTASH_REDIS_REST_URL`),
+  token: getEnvVariable(`UPSTASH_REDIS_REST_TOKEN`),
+});
